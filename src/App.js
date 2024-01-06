3d77772aceae443711c04f048291c07c4621832d
Varshith R <varshithvarshi8613@gmail.com>
2024-01-06 13:04:01 +0530

    Added few components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
-import React from 'react'
-import {Switch , Route, Link} from 'react-router-dom'
-import {Layout , Typography , Space} from 'antd'
-
-
+import React from "react";
+import { Switch, Route, Link, Routes } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Exchanges,
+  HomePage,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
+import "./App.css";
 const App = () => {
   return (
-    <div className='app'>
-       <div className='navbar'>
-        <div className='main'>
-            <div className='footer'>
-                
-            </div>
-        </div>
-       </div>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" exact element={<HomePage />} />
+              <Route path="/exchanges" exact element={<Exchanges />} />
+              <Route
+                path="/cryptocurrencies"
+                exact
+                element={<Cryptocurrencies />}
+              />
+              <Route path="/crypto/:coinId" exact element={<CryptoDetails />} />
+              <Route path="/news" exact element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      <div className="footer" >
+        <Typography.Title level={5} style={{color:"white", textAlign:"center"}}>
+            Cryptoverse <br/>
+            All rights reserved
+        </Typography.Title>
+        <Space>
+            <Link to='/'>Home</Link>
+            <Link to='/exchanges'>Exchanges</Link>
+            <Link to='/news'>News</Link>
+        </Space>
+      </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';  // Replace with the path to your main component
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = document.getElementById('root');
 
 const rootElement = createRoot(root);
 rootElement.render(
-
+  <Router>
     <App />
-  
+  </Router>
 );
